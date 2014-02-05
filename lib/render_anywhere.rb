c20cf71361ff2b3a86c40aaadda9ddb45d2a90a8
spodlecki <spodlecki@skintertainment.com>
2014-02-05 08:11:41 -0600

    Add options to include additional helpers and possibly instance variables

--- lib/render_anywhere.rb
+++ lib/render_anywhere.rb
@@ -8,7 +8,19 @@ module RenderAnywhere
   def render(*args)
     rendering_controller.render_to_string(*args)
   end
-  
+  def set_render_anywhere_helpers(*args)
+    args.each do |helper_name|
+      rendering_controller.class_eval do
+        helper helper_name.to_s.constantize
+      end
+    end
+  end
+  def set_instance_variable(var, value)
+    rendering_controller.class_eval do
+      attr_accessor :"#{var}"
+    end
+    rendering_controller.public_send("#{var}=", value)
+  end
   def rendering_controller
     @rendering_controller ||= RenderAnywhere::RenderingController.new
   end
